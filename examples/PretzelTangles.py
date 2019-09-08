06b546876372763fbca3c1952ce1c654b034b03c
Gurkeerat <gurkeerat.chhina@gmail.com>
2019-09-08 10:58:30 -0700

    computed pretzel tangles

--- examples/PretzelTangles.py
+++ examples/PretzelTangles.py
@@ -1,7 +1,109 @@
-field = 2
+field = 5
 
 name1 = "PretzelTangles/(2,-3)"
 Tangle1 = Tangle.PretzelTangle(2, -3)
 BNcx1 = Tangle1.toReduced_BNComplex(1000, field)
 multicurve1 = BNcx1.to_multicurve()
 multicurve1.drawpng(name1+"_BNr","hdelta",Tangle1.slices)
+
+name2 = "PretzelTangles/(2,2)"
+Tangle2 = Tangle.PretzelTangle(2, 2)
+BNcx2 = Tangle2.toReduced_BNComplex(1000, field)
+multicurve2 = BNcx2.to_multicurve()
+multicurve2.drawpng(name2+"_BNr","hdelta",Tangle2.slices)
+
+name3 = "PretzelTangles/(2,3)"
+Tangle3 = Tangle.PretzelTangle(2, 3)
+BNcx3 = Tangle3.toReduced_BNComplex(1000, field)
+multicurve3 = BNcx3.to_multicurve()
+multicurve3.drawpng(name3+"_BNr","hdelta",Tangle3.slices)
+
+name4 = "PretzelTangles/(2,-4)"
+Tangle4 = Tangle.PretzelTangle(2, -4)
+BNcx4 = Tangle4.toReduced_BNComplex(1000, field)
+multicurve4 = BNcx4.to_multicurve()
+multicurve4.drawpng(name4+"_BNr","hdelta",Tangle4.slices)
+
+name5 = "PretzelTangles/(2,-5)"
+Tangle5 = Tangle.PretzelTangle(2, -5)
+BNcx5 = Tangle5.toReduced_BNComplex(1000, field)
+multicurve5 = BNcx5.to_multicurve()
+multicurve5.drawpng(name5+"_BNr","hdelta",Tangle5.slices)
+
+name6 = "PretzelTangles/(2,-6)"
+Tangle6 = Tangle.PretzelTangle(2, -6)
+BNcx6 = Tangle6.toReduced_BNComplex(1000, field)
+multicurve6 = BNcx6.to_multicurve()
+multicurve6.drawpng(name6+"_BNr","hdelta",Tangle6.slices)
+
+name7 = "PretzelTangles/(2,-7)"
+Tangle7 = Tangle.PretzelTangle(2, -7)
+BNcx7 = Tangle7.toReduced_BNComplex(1000, field)
+multicurve7 = BNcx7.to_multicurve()
+multicurve7.drawpng(name7+"_BNr","hdelta",Tangle7.slices)
+
+name8 = "PretzelTangles/(3,-3)"
+Tangle8 = Tangle.PretzelTangle(3, -3)
+BNcx8 = Tangle8.toReduced_BNComplex(1000, field)
+multicurve8 = BNcx8.to_multicurve()
+multicurve8.drawpng(name8+"_BNr","hdelta",Tangle8.slices)
+
+name9 = "PretzelTangles/(3,-4)"
+Tangle9 = Tangle.PretzelTangle(3, -4)
+BNcx9 = Tangle9.toReduced_BNComplex(1000, field)
+multicurve9 = BNcx9.to_multicurve()
+multicurve9.drawpng(name9+"_BNr","hdelta",Tangle9.slices)
+
+name10 = "PretzelTangles/(3,-5)"
+Tangle10 = Tangle.PretzelTangle(3, -5)
+BNcx10 = Tangle10.toReduced_BNComplex(1000, field)
+multicurve10 = BNcx10.to_multicurve()
+multicurve10.drawpng(name10+"_BNr","hdelta",Tangle10.slices)
+
+name11 = "PretzelTangles/(3,-6)"
+Tangle11 = Tangle.PretzelTangle(3, -6)
+BNcx11 = Tangle11.toReduced_BNComplex(1000, field)
+multicurve11 = BNcx11.to_multicurve()
+multicurve11.drawpng(name11+"_BNr","hdelta",Tangle11.slices)
+
+name12 = "PretzelTangles/(3,-7)"
+Tangle12 = Tangle.PretzelTangle(3, -7)
+BNcx12 = Tangle12.toReduced_BNComplex(1000, field)
+multicurve12 = BNcx12.to_multicurve()
+multicurve12.drawpng(name12+"_BNr","hdelta",Tangle12.slices)
+
+name13 = "PretzelTangles/(4,-3)"
+Tangle13 = Tangle.PretzelTangle(4, -3)
+BNcx13 = Tangle13.toReduced_BNComplex(1000, field)
+multicurve13 = BNcx13.to_multicurve()
+multicurve13.drawpng(name13+"_BNr","hdelta",Tangle13.slices)
+
+name14 = "PretzelTangles/(4,-5)"
+Tangle14 = Tangle.PretzelTangle(4, -5)
+BNcx14 = Tangle14.toReduced_BNComplex(1000, field)
+multicurve14 = BNcx14.to_multicurve()
+multicurve14.drawpng(name14+"_BNr","hdelta",Tangle14.slices)
+
+name15 = "PretzelTangles/(4,-7)"
+Tangle15 = Tangle.PretzelTangle(4, -7)
+BNcx15 = Tangle15.toReduced_BNComplex(1000, field)
+multicurve15 = BNcx15.to_multicurve()
+multicurve15.drawpng(name15+"_BNr","hdelta",Tangle15.slices)
+
+name16 = "PretzelTangles/(6,-7)"
+Tangle16 = Tangle.PretzelTangle(6, -7)
+BNcx16 = Tangle16.toReduced_BNComplex(1000, field)
+multicurve16 = BNcx16.to_multicurve()
+multicurve16.drawpng(name16+"_BNr","hdelta",Tangle16.slices)
+
+name17 = "PretzelTangles/(2,-2)"
+Tangle17 = Tangle.PretzelTangle(2, -2)
+BNcx17 = Tangle17.toReduced_BNComplex(1000, field)
+multicurve17 = BNcx17.to_multicurve()
+multicurve17.drawpng(name17+"_BNr","hdelta",Tangle17.slices)
+
+name18 = "PretzelTangles/(3,3)"
+Tangle18 = Tangle.PretzelTangle(3, 3)
+BNcx18 = Tangle18.toReduced_BNComplex(1000, field)
+multicurve18 = BNcx18.to_multicurve()
+multicurve18.drawpng(name18+"_BNr","hdelta",Tangle18.slices)
